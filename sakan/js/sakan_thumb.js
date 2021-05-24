44d2535a50e66d51c48086836632b9e09ef88e6c
kkkkanata <romaborgiasprtukus.gmail.com>
2021-05-25 01:20:09 +0900

    add sakan

--- sakan/js/sakan_thumb.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  const thumbs = document.querySelectorAll('.thumb');
-  thumbs.forEach(function(item){
- item.onclick = function(){
-  document.getElementById('bigimg').src = this.dataset.image;
- }
-});
-})
